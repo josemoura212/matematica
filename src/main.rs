1a7a0ca038d0d54fd90c47ea81e80e72e4554156
José Augusto <josemoura212@gmail.com>
2024-01-08 16:49:46 -0300

    Update function calls in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use jose_functions::{somar, subtrair};
 
 fn main() {
-    println!("2 + 2: {}", somar(2, 2));
-    println!("2 - 2: {}", subtrair(2, 2));
+    println!("Somar - 2 + 2: {}", somar(2, 2));
+    println!("Subtratir - 2 - 2: {}", subtrair(2, 2));
 }
